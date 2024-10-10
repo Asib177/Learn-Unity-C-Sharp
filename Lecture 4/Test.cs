4589c5d76e9ef56ead0964d4847614456e5c393c
Md. Rakibul Asib Redoy <126803555+Asib177@users.noreply.github.com>
2024-10-10 15:08:35 +0600

    Add files via upload

---
+++ Lecture 4/Test.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    //Rigidbody rb;
+    // Start is called before the first frame update
+    public GameObject winText;
+    
+    void Start()
+    {
+        //Destroy(gameObject);
+        //Destroy(gameObject, 3f);
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }*/
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * 500);
+        }*/
+        //rb.velocity = Vector3.forward * 20f;
+        /*if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level2");
+        }*/
+    }
+
+    /*private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }*/
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            //Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            //Destroy(gameObject);
+            Destroy(collision.gameObject);
+            winText.SetActive(true);
+        }
+    }
+}
